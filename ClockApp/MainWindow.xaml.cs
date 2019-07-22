f5529c3c8a0fcbe79678017aaa1151669c67169a
Grin <grinefimov@yandex.ru>
2019-07-22 17:54:24 +0300

    Set up window
    * Main window with minimize, to tray (+context menu), close buttons
    * 3 tabs
    * Dark theme

--- ClockApp/MainWindow.xaml.cs
+++ ClockApp/MainWindow.xaml.cs
@@ -1,17 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MouseEventArgs = System.Windows.Forms.MouseEventArgs;
 
 namespace ClockApp
 {
@@ -20,12 +23,29 @@ namespace ClockApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private System.Windows.Forms.NotifyIcon notifier = new System.Windows.Forms.NotifyIcon();
         public MainWindow()
         {
+            this.Title = "Clock";
+
             InitializeComponent();
+
+            notifier.Icon = new System.Drawing.Icon("alarm.ico"); // this.notifier.Icon = ForumProjects.Properties.Resources.A;
+            notifier.Visible = false;
+            notifier.MouseClick += OpenWindow;
+            notifier.MouseDown += OpenNotifierContextMenu;
         }
 
-        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        private void OpenNotifierContextMenu(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                System.Windows.Controls.ContextMenu menu = (System.Windows.Controls.ContextMenu)this.FindResource("NotifierContextMenu");
+                menu.IsOpen = true;
+            }
+        }
+
+        private void DragMove(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
                 this.DragMove();
@@ -33,7 +53,35 @@ namespace ClockApp
 
         private void CloseApp(object sender, RoutedEventArgs e)
         {
-            Application.Current.Shutdown();
+            System.Windows.Application.Current.Shutdown();
+        }
+
+        private void MinimizeApp(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void HideWindow(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            notifier.Visible = true;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            notifier.Visible = false;
+        }
+
+        private void OpenWindow(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                this.Show();
+                this.WindowState = WindowState.Normal;
+                Icon.Focus();
+                notifier.Visible = false;
+            }
+            
         }
     }
 }
